89d2dce8da084eb2efc94b78606966249eb1099b
lukasz-pawlos <252925@student.pwr.edu.pl>
2022-08-06 23:13:47 +0200

    First steps in Vue with TS

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
+  <Home/>
   <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
 import HelloWorld from './components/HelloWorld.vue';
+import Home from './components/Home.vue';
 
 @Options({
   components: {
     HelloWorld,
+    Home,
   },
 })
 export default class App extends Vue {}
